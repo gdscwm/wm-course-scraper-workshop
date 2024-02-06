19b16bc870d0aa748b56a11c9a1544d85161c679
Colleen Polka <colleenpolka@chicken-big.local>
2024-02-06 18:57:06 -0500

    Remove .js files

--- wm-course-scraper/src/enums/status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status["ALL"] = "0";
-    Status["OPEN"] = "OPEN";
-    Status["CLOSED"] = "CLOSED";
-})(Status || (exports.Status = Status = {}));
